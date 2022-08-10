e8082c710848d6895a2bedfd1d598423b23e360f
Mandla Ndzimakhwe <smindlondz@gmail.com>
2022-08-10 21:00:10 +0200

    initial commit

---
+++ bb.js
@@ -0,0 +1,6 @@
+
+// import the greet module that is in the current folder using CommonJS
+const greet = require('./aa')
+
+// call the function & print the result on the screen
+console.log(greet('Xola'))
